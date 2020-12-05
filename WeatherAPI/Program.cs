c77165c3b41eb04c534d3db9ce6990706170f732
Chirag Mehta <cm298@students.waikato.ac.nz>
2020-12-05 18:02:55 +1300

    Add project files.

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace WeatherAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is the name of your city?");
+            string cityname = Console.ReadLine();
+
+            //bd85e0c5d55db50e3d5f0d8fb566ef33
+            String test = Get(Url(cityname));
+            Console.WriteLine(test);
+        }
+
+        public static string Url(string cityname)
+        {
+            string url;
+            url = "api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid="; //Super secret API key
+
+            return url;
+
+        }
+
+        public static string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" + uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
